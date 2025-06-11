76dfe33f64f68a62d916586b1fc77c3a5c0c6013
Your Name <you@example.com>
2025-06-11 15:34:52 -0400

    ADD : se añadió la dependencia de H2 para pruebas y se actualizó el archivo .gitignore para incluir archivos .env. Además, se activó el perfil de pruebas en ClientesApplicationTests.

--- src/test/java/com/perfulandia/cl/clientes/ClientesApplicationTests.java
+++ src/test/java/com/perfulandia/cl/clientes/ClientesApplicationTests.java
@@ -2,8 +2,11 @@ package com.perfulandia.cl.clientes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClientesApplicationTests {
 
 	@Test
